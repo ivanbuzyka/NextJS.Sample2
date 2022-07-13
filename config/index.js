dc8ded7b119cc5171c5d31f0be148eb823bd0b36
ivanbuzyka <ivanbuzyka@gmail.com>
2022-07-13 15:26:40 +0200

    added correct prod server

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'https://62cec64c826a88972d02c65d.mockapi.io'
